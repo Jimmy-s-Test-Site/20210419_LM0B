ad6bbb3870318e6db9e59e88225ccb90840e24b0
jaacko-torus <jaacko.torus@gmail.com>
2021-04-19 19:23:19 -0400

    added entered portal signal

--- Julian/Portal/Portal.gd
+++ Julian/Portal/Portal.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+signal entered_portal
+
 func _process(delta : float) -> void:
 	var velocity = self.get_global_mouse_position() - self.global_position
 	self.global_position += velocity
+
+func _on_CollisionArea_area_entered(area : Area2D) -> void:
+	self.emit_signal("entered_portal")
