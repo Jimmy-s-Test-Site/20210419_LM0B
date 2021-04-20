a1e40c0539403e3fc862d02255f43ed2cd4a1f23
jaacko-torus <jaacko.torus@gmail.com>
2021-04-20 11:53:54 -0400

    Variable renaming in `RotatingGravityArea.gd`
    `area_gravity_nodes` -> `gravity_area_nodes`

--- Julian/RotatingGravityArea/RotatingGravityArea.gd
+++ Julian/RotatingGravityArea/RotatingGravityArea.gd
@@ -1,14 +1,14 @@
 extends Area2D
 
-export(Array, Resource) var area_gravity_nodes
+export(Array, Resource) var gravity_area_nodes
 
 export(bool) var enabled
 
 func _ready() -> void:
 	var is_first : bool = true
 	
-	for area_gravity_node_iteration in range(self.area_gravity_nodes.size()):
-		var area_gravity_node = self.area_gravity_nodes[area_gravity_node_iteration]
+	for area_gravity_node_iteration in range(self.gravity_area_nodes.size()):
+		var area_gravity_node = self.gravity_area_nodes[area_gravity_node_iteration]
 		
 		var timer : Timer = Timer.new()
 		timer.set_name(str("Timer", area_gravity_node_iteration))
@@ -32,7 +32,7 @@ func timer_callback(index : int, gravity_vector : Vector2) -> void:
 	if enabled:
 		self.gravity_vec = gravity_vector
 		
-		var next_timer_index : int = (index + 1) % self.area_gravity_nodes.size()
+		var next_timer_index : int = (index + 1) % self.gravity_area_nodes.size()
 		var next_timer_name : String = str("Timer", next_timer_index)
 		var next_timer_path : String = str("Timers/", next_timer_name)
 		self.get_node(next_timer_path).start()
