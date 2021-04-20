97e6a20051bc7da33490dd3f93160d04233e572f
Mike Sroka <mikesroka97@gmail.com>
2021-04-20 18:42:08 -0500

    started player push mechanic

--- Mike/Player/Player.gd
+++ Mike/Player/Player.gd
@@ -2,6 +2,31 @@ extends RigidBody2D
 
 export (int) var movementSpeed = 200
 export (int) var pushSpeed = 500
+export (int) var xStart = 0
+export (int) var yStart = 0
+
+onready var just_aired_timer : Timer = $JustAiredTimer
+onready var _transitions: = {
+	IDLE: [RUN, AIR],
+	RUN: [IDLE, AIR],
+	AIR: [IDLE],
+}
+
+const FLOOR_NORMAL := Vector2.UP
+
+enum {
+	IDLE,
+	RUN,
+	AIR,
+}
+
+var _state: int = IDLE
+
+var states_strings := {
+	IDLE: "idle",
+	RUN: "run",
+	AIR: "air",
+}
 
 var velocity = Vector2()
 
@@ -15,9 +40,14 @@ func get_player_input():
 		velocity.x += pushSpeed
 	if Input.is_action_pressed("push_right"):
 		velocity.x -= pushSpeed
+	
 	velocity = velocity.normalized() * movementSpeed
 	
-func _integrate_forces(state):
+	
+	
+	
+func _integrate_forces(state: Physics2DDirectBodyState) -> void:
+	#var is_touching_surface := state.get_contact_count() > 0
 	get_player_input()
 	self.apply_impulse(Vector2.ZERO, velocity)
 
