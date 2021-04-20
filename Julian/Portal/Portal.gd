2203591507e0524d6a4568811078ce2de40682af
jaacko-torus <jaacko.torus@gmail.com>
2021-04-19 20:26:00 -0400

    added export for portal follow mouse

--- Julian/Portal/Portal.gd
+++ Julian/Portal/Portal.gd
@@ -2,9 +2,12 @@ extends Area2D
 
 signal entered_portal
 
-func _process(delta : float) -> void:
-	var velocity = self.get_global_mouse_position() - self.global_position
-	self.global_position += velocity
+export(bool) var follow_player = false
 
-func _on_CollisionArea_area_entered(area : Area2D) -> void:
+func _process(_delta : float) -> void:
+	if self.follow_player:
+		var velocity = self.get_global_mouse_position() - self.global_position
+		self.global_position += velocity
+
+func _on_CollisionArea_body_entered(_body : Node) -> void:
 	self.emit_signal("entered_portal")
