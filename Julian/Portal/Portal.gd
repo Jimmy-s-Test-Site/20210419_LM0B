021c2fc5d1dfedf1660fcef2b158b7d37180f409
jaacko-torus <jaacko.torus@gmail.com>
2021-04-19 21:38:02 -0400

    simplified cursor following logic in `Portal.gd`

--- Julian/Portal/Portal.gd
+++ Julian/Portal/Portal.gd
@@ -6,8 +6,7 @@ export(bool) var follow_player = false
 
 func _process(_delta : float) -> void:
 	if self.follow_player:
-		var velocity = self.get_global_mouse_position() - self.global_position
-		self.global_position += velocity
+		self.global_position = self.get_global_mouse_position()
 
 func _on_CollisionArea_body_entered(_body : Node) -> void:
 	self.emit_signal("entered_portal")
